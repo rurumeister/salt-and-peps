45fad7b89d127e2a9e0320b559f319468063d326
elroychua <elroymx@gmail.com>
2024-05-23 23:27:08 +0800

    chore: refine next config to follow current protocols

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ap-south-1.graphassets.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ap-south-1.graphassets.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
+//    domains: ["ap-south-1.graphassets.com"],
 export default nextConfig;
